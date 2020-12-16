c74417b76ff5f6b3dab95231c096666edec07469
Natnael Tezera <natnaeltezera@outlook.com>
2020-12-16 15:29:32 -0800

    screen shot and product controller modified

--- ProductRestApi/src/main/java/com/ntcasestudy/productrestapi/controller/ProductController.java
+++ ProductRestApi/src/main/java/com/ntcasestudy/productrestapi/controller/ProductController.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.ntcasestudy.productrestapi.exception.ResourceNotFoundException;
 import com.ntcasestudy.productrestapi.model.Product;
 import com.ntcasestudy.productrestapi.service.ProductService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/product")
 public class ProductController {
